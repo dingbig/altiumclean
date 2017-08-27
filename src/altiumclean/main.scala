e14e5c7ad30a4868343998820329a94bc6340040
dingbig <dingbig@gmail.com>
2017-08-27 19:55:55 +0800

    Initial commit

---
+++ src/altiumclean/main.scala
@@ -0,0 +1,34 @@
+package altiumclean
+
+import java.io.File
+
+object main extends App {
+  def process_dir(dir: File): Unit = {
+    if(isAltiumProject(dir)) {
+      cleanAltiumProject(dir)
+    }
+
+    dir.listFiles().filter(_.isDirectory).foreach(process_dir(_))
+  }
+
+  def isAltiumProject(dir: File): Boolean = dir.listFiles().exists(_.getAbsolutePath.toLowerCase().endsWith(".prjpcb"))
+  def cleanAltiumProject(dir: File) ={
+    val subFiles = dir.listFiles()
+    subFiles.filter(_.getName.startsWith("Project Outputs ")).foreach(rmrf)
+    subFiles.filter(_.getName == ("History")).foreach(rmrf)
+    subFiles.filter(_.getName == ("__Previews")).foreach(rmrf)
+
+
+  }
+  def rmrf(f: File): Unit = {
+    if(f.isDirectory) {
+      f.listFiles().foreach(rmrf(_))
+    }
+    f.delete()
+    println(f + " deleted")
+
+  }
+  val dirs = if(args.length == 0) List(new File(System.getProperty("user.dir"))) else args.map(new File(_)).toList
+  dirs.foreach(process_dir(_))
+
+}
